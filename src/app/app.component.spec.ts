fa4aa2420547369343ae204cfcfa71b870b7e404
Zak <zakary.howard94@gmail.com>
2018-10-06 18:53:59 +0200

    problem with rendering title. Hard coded for now. All tests green

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { Input } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -7,14 +9,17 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        FormsModule
+      ],
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
+  }));
 
   it(`should have as title 'address-book-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -28,4 +33,6 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to address-book-angular!');
   });
+
+  it('createContact should add contact to contacts')
 });
